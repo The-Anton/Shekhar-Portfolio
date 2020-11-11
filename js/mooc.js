d7bf857f0b454021296608c8b597e5d3ced59c4c
The-Anton <shekhar59324@gmail.com>
2020-11-11 13:15:23 +0530

    Intro section added

--- js/mooc.js
+++ js/mooc.js
@@ -2,7 +2,7 @@ function loadImage(i){
     document.getElementById('certificate-area-card').innerHTML=`
                                         <div class="card">
                                         <div class="card-body">
-                                        <img src="media/${i}"/>
+                                        <img data-src="media/${i}"/>
                                         </div>
                                         </div>`
 }
