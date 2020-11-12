bfa70433083df622c188f2d3a9fe85543c6071eb
The-Anton <shekhar59324@gmail.com>
2020-11-12 19:36:06 +0530

    Remove lazing loading

--- js/mooc.js
+++ js/mooc.js
@@ -2,7 +2,7 @@ function loadImage(i){
     document.getElementById('certificate-area-card').innerHTML=`
                                         <div class="card">
                                         <div class="card-body">
-                                        <img data-src="media/${i}"/>
+                                        <img src="media/${i}"/>
                                         </div>
                                         </div>`
 }
